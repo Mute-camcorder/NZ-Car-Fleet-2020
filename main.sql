9db0500fc9c5a45ed15194d851d14cba407666bb
Cam Smith <CameronL.Smith@countdown.co.nz>
2022-08-24 14:39:02 +1200

    Add basic main.sql with two open questions.

---
+++ main.sql
@@ -0,0 +1,16 @@
+.mode column
+.open nz-fleet-2020.sqlite3
+
+-- You can read this file with the command `.read main.sql`
+-- Inspect the schema of the `fleet` table and take a look at some of the data
+  
+
+-- Which month had the most first-time registrations? Which one had the least? Can you write a query to display all months?
+
+SELECT 1; -- complete query here
+
+
+-- Cars can have many different fuel sources, this is recorded in the MOTIVE_POWER column. Are there any trends across the year with regards to the proportion of cars that were non-petrol powered?
+-- Treat DIESEL, PETROL, and "" as being the same for this task.
+
+SELECT 1; -- complete query here
